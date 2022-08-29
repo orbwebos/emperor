34b77b586ced4668dfb1a097d8a5693fd98a95a6
Orbwebos <97360590+Orbwebos@users.noreply.github.com>
2022-08-28 18:52:27 -0700

    generalize the bot owner ID check

--- src/util/owner.ts
+++ src/util/owner.ts
@@ -1,7 +1,6 @@
+import { config } from './config_manager';
+import { isInArray } from './is_in';
+
 export function isOwnerId(id: string): boolean {
-  return (
-    id === '327552417166131200' ||
-    id === '497662207425904650' ||
-    id === '461911279867527190'
-  );
+  return isInArray(config.bot.ownerIds, id);
 }
