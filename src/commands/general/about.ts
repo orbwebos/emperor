dde4076d158c150506bdf1a365cd3d3ab6b02e07
Orbwebos <97360590+Orbwebos@users.noreply.github.com>
2022-10-21 12:39:00 -0700

    create `about` command

---
+++ src/commands/general/about.ts
@@ -0,0 +1,68 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+import { Message, MessageEmbed } from 'discord.js';
+
+function milisecondsToDhms(time: number) {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((time % (1000 * 60)) / 1000);
+
+  const daysString = `${days} day${days === 1 ? '' : 's'}`;
+  const hoursString = `${hours} hour${hours === 1 ? '' : 's'}`;
+  const minutesString = `${minutes} minute${minutes === 1 ? '' : 's'}`;
+  const secondsString = `${seconds} second${seconds === 1 ? '' : 's'}`;
+
+  const timeString = [daysString, hoursString, minutesString, secondsString]
+    .filter((string) => string.startsWith('0') === false)
+    .join(', ');
+
+  return timeString;
+}
+
+@ApplyOptions<Command.Options>({
+  description: 'Displays information about the bot.',
+})
+export class UserCommand extends Command {
+  public async messageRun(message: Message) {
+    const { config } = this.container;
+
+    const guilds = this.container.client.guilds.cache.size;
+    const formattedUptime = milisecondsToDhms(this.container.client.uptime);
+
+    const embed = new MessageEmbed()
+      .setColor('#7850bd')
+      .setThumbnail(this.container.client.user.displayAvatarURL())
+      .setTitle(`About ${config.bot.name}`)
+      .addFields(
+        { name: 'Version', value: config.bot.version, inline: true },
+        {
+          name: 'Servers',
+          value: guilds === 1 ? `${guilds} server` : `${guilds} servers`,
+          inline: true,
+        },
+        {
+          name: 'Uptime',
+          value: formattedUptime,
+          inline: true,
+        },
+        {
+          name: 'Library',
+          value: '[discord.js](https://discord.js.org)',
+          inline: true,
+        },
+        {
+          name: 'Source code',
+          value: '[GitHub](https://github.com/orbwebos/emperor)',
+          inline: true,
+        },
+        {
+          name: 'License',
+          value: '[MIT](https://spdx.org/licenses/MIT.html)',
+          inline: true,
+        }
+      );
+
+    message.reply({ embeds: [embed] });
+  }
+}
