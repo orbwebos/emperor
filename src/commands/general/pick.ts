fc949d74de313ae4c55d2f639ce5662eb07c6dce
Orbwebos <97360590+Orbwebos@users.noreply.github.com>
2022-10-21 12:39:38 -0700

    create `pick` command

---
+++ src/commands/general/pick.ts
@@ -0,0 +1,37 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Args, Command } from '@sapphire/framework';
+import { Message, MessageEmbed } from 'discord.js';
+
+@ApplyOptions<Command.Options>({
+  description: 'Makes a choice between multiple options.',
+  aliases: ['choice', 'chose'],
+})
+export class PickCommand extends Command {
+  private parseChoices(options: string) {
+    return options.split(/,|\|/).map((option) => option.trim());
+  }
+
+  public async messageRun(message: Message, args: Args) {
+    const options = await args.rest('string');
+    const choices = this.parseChoices(options);
+
+    if (choices.length === 0) {
+      return message.reply('You must provide at least one option');
+    }
+
+    const embed = new MessageEmbed()
+      .setTitle(choices[Math.floor(Math.random() * choices.length)])
+      .setDescription(
+        `The above was picked from the following choices: \`${choices.join(
+          '`, `'
+        )}\``
+      )
+      .setColor(this.container.config.bot.defaultColor)
+      .setFooter({
+        text: `Requested by ${message.author.tag}`,
+        iconURL: message.author.displayAvatarURL(),
+      });
+
+    return message.reply({ embeds: [embed] });
+  }
+}
